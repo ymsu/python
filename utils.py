210f39a083d7dd45d720f5f117cd730806881b71
ymsu <1102190566@qq.com>
2019-05-17 09:15:49 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,7 +7,9 @@ import os
 # 线程类
 
 class FuncThread(threading.Thread):
-    
+    """
+    自己定义的线程类
+    """
     def __init__(self, func, *args, **kwargs):
         super(FuncThread, self).__init__()
         self.func = func
@@ -34,6 +36,10 @@ class FuncThread(threading.Thread):
 # 函数执行超时时间判断
 
 def handle_timeout(func, timeout, *args, **kwargs):
+    """
+    在一段时间（超时时间 timeout）内检查某一件事情（函数）是否完成，
+    如果完成，则拿到返回的结果并退出超时，如果没有完成，返回 False 或者 None, 代表这件事情在超时时间内没有完成 （timeout）
+    """
     interval = 1
     
     ret = None
